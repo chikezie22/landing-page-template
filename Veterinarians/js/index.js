e536ef3181470af4e618bc41224d4b385394ce7c
chikezie22 <simonchikezie44@gmail.com>
2025-01-28 15:15:10 +0100

    Vertinaries Update

--- Veterinarians/js/index.js
+++ Veterinarians/js/index.js
@@ -1,15 +1,15 @@
 const arrow = document.querySelector('#arrow');
 const nurse = document.querySelector('#nurse');
 
-const slide = () => {
-  console.log(arrow);
-  setTimeout(() => {
-    arrow.classList.remove('animate-slide-right');
-    nurse.classList.remove('animate-slide-left');
-    arrow.offsetWidth;
-    arrow.classList.add('animate-slide-right');
-    nurse.classList.add('animate-slide-left');
-    slide();
-  }, 10000);
-};
-window.addEventListener('load', slide);
+// const slide = () => {
+//   console.log(arrow);
+//   setTimeout(() => {
+//     arrow.classList.remove('animate-slide-right');
+//     nurse.classList.remove('animate-slide-left');
+//     arrow.offsetWidth;
+//     arrow.classList.add('animate-slide-right');
+//     nurse.classList.add('animate-slide-left');
+//     slide();
+//   }, 10000);
+// };
+// window.addEventListener('load', slide);
