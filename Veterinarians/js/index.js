9ba3dd8db3f00f9259adbc9074441d73f8b9f70b
chikezie22 <simonchikezie44@gmail.com>
2025-02-03 11:38:04 +0100

    added play and pause functionality

--- Veterinarians/js/index.js
+++ Veterinarians/js/index.js
@@ -1,15 +1,37 @@
 const arrow = document.querySelector('#arrow');
 const nurse = document.querySelector('#nurse');
+const video = document.querySelector('.video');
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
 
-// const slide = () => {
-//   console.log(arrow);
-//   setTimeout(() => {
-//     arrow.classList.remove('animate-slide-right');
-//     nurse.classList.remove('animate-slide-left');
-//     arrow.offsetWidth;
-//     arrow.classList.add('animate-slide-right');
-//     nurse.classList.add('animate-slide-left');
-//     slide();
-//   }, 10000);
-// };
-// window.addEventListener('load', slide);
+const slide = () => {
+  console.log(arrow);
+  setTimeout(() => {
+    arrow.classList.remove('animate-slide-right');
+    nurse.classList.remove('animate-slide-left');
+    arrow.offsetWidth;
+    arrow.classList.add('animate-slide-right');
+    nurse.classList.add('animate-slide-left');
+    slide();
+  }, 10000);
+};
+window.addEventListener('load', slide);
+
+play.addEventListener('click', (e) => {
+  e.preventDefault();
+  video.play();
+  play.classList.toggle('hidden');
+  pause.classList.remove('hidden');
+});
+
+pause.addEventListener('click', (e) => {
+  e.preventDefault();
+  video.pause();
+  pause.classList.toggle('hidden');
+  play.classList.toggle('hidden');
+});
+
+video.addEventListener('ended', () => {
+  video.currentTime = 0;
+  video.play();
+});
