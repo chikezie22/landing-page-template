a4eee1bc4d0741369c4c826b80b9f1ed188a2cc8
churchill nnanna <mrxyfactor@gmail.com>
2025-04-03 18:13:59 +0100

    added slide feauture for text

--- kash-juice/app.js
+++ kash-juice/app.js
@@ -1,21 +1,51 @@
 
+const root = document.documentElement
+const rootStyles = getComputedStyle(root);
+// const dynamicBtnBorderColor = rootStyles.getPropertyValue('--dynamic-btn-border-color').trim();
+// const dynamicHeroBgColor = rootStyles.getPropertyValue('--dynamic-hero-bg-color').trim();
 
+const btnBorderColors=[
+    '--btn-border-orange',
+    '--btn-border-strawberry',
+    '--btn-border-blueberry',
+]
+const btnBorderHex= btnBorderColors.map(color=>rootStyles.getPropertyValue(color).trim())
 
-//handle text change
+const heroBgColors=[
+    '--hero-bg',
+    '--strawberry-text',
+    '--blueberry-text',
+]
+const heroBgHex= heroBgColors.map(color=>rootStyles.getPropertyValue(color).trim())
 
+const juiceSlides= document.querySelector('#juice-slides');
+const juiceSlideWidth= juiceSlides.offsetWidth;
+console.log(juiceSlideWidth);
+const parentText= document.querySelector('#parent-text');
+const slidesWrapper= document.querySelector('#slides-wrapper').offsetHeight;
+const slides= document.querySelector('#slides');
+const slideElementsWidth= Array.from(slides.children).map(child=>child.offsetWidth);
+// const heroTryItBtn= document.querySelector('#hero-try-it-btn');
+// const heroTransitionedBg= document.querySelector('#hero-transitioned-bg');
 
+parentText.firstElementChild.innerText= ''
+parentText.style.paddingLeft= `${slideElementsWidth[0]}px`;
+setTimeout(() => {
+    parentText.style.transition=`padding 1s ease-in-out`;
+}, 1000)
 
-const slideTexts= document.querySelector('#slide-text');
-const changedText= document.querySelector('#changed-text');
-console.log(changedText);
-const height= slideTexts.parentElement.offsetHeight;
-
+const height= slides.parentElement.offsetHeight;
+console.log(slideElementsWidth);
 let index=1
-console.log(slideTexts, height);
+console.log(slides, height);
 setInterval(() => {
-    changedText.innerText=slideTexts.children[index].innerText;
-    console.log(changedText.innerText);
-    slideTexts.style.transform=`translateY(-${height *index}px)`;
+    slides.style.transform=`translateY(-${height *index}px)`;
+    parentText.style.paddingLeft= `${slideElementsWidth[index]}px`;
+    document.documentElement.style.setProperty('--dynamic-hero-bg-color', heroBgHex[index]);
+    document.documentElement.style.setProperty('--dynamic-btn-border-color', btnBorderHex[index]);
+    juiceSlides.c
+    juiceSlides.style.transform=`translateX(-${juiceSlides.parentElement.offsetWidth *index}px)`;
+
+    
     index == 2 ? index=0 :index++;
-    // index++
 }, 5000);
