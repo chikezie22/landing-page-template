7372d0d7ea46c43f162901c4e40d31b3f72eb3fb
chikezie22 <simonchikezie44@gmail.com>
2025-04-28 17:07:23 +0100

    Add new SVG icons for explore and shopping bag
    - Added explore.svg with a detailed path for the explore icon.
    - Added shopping-bag.svg with paths for the shopping bag icon, including details for the bag and handle.

---
+++ SkinCare/js/index.js
@@ -0,0 +1,22 @@
+// first section
+document.addEventListener('DOMContentLoaded', function () {
+  const images = document.querySelectorAll('.image-slide');
+  console.log(images);
+  let current = 0;
+  function showImage(index) {
+    images.forEach((img) => {
+      img.classList.add('hidden');
+      img.classList.remove('animate-slide-up');
+    });
+    images[index].classList.remove('hidden');
+    images[index].classList.add('animate-slide-up');
+  }
+
+  function nextImage() {
+    current = (current + 1) % images.length;
+    showImage(current);
+  }
+
+  showImage(current);
+  setInterval(nextImage, 3000);
+});
